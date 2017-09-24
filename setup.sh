4c266a70cc0c4060aa035f2e86c37e32c829071c
Mike Wu <me@mikewuis.me>
2017-09-23 17:20:48 -0700

    Added more files to download using setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,10 @@
 FILE=./download.log
 
 # url to retrieve
-TRAIN_URL=https://www.dropbox.com/s/h5yora9j0onglw6/train.zip?dl=0
-TEST_URL=https://www.dropbox.com/s/c94io61nmldcgv8/test.zip?dl=0
+TRAIN_IMG_URL=https://www.dropbox.com/s/h5yora9j0onglw6/train.zip?dl=0
+TRAIN_EMB_URL=https://www.dropbox.com/s/wnqdznmlhkybfbo/train_emb.zip?dl=0
+TEST_IMG_URL=https://www.dropbox.com/s/c94io61nmldcgv8/test.zip?dl=0
+TEST_EMB_URL=https://www.dropbox.com/s/l0n37n0ryy0o4ca/test_emb.zip?dl=0
 
 # write header information to the log file
 start_date=`date`
@@ -13,8 +15,10 @@ echo "START-------------------------------------------------" >> $FILE
 echo "" >> $FILE
 
 # retrieve the web page using curl. time the process with the time command.
-time (curl --connect-timeout 100 $TRAIN_URL) >> $FILE 2>
-time (curl --connect-timeout 100 $TEST_URL) >> $FILE 2>
+time (curl --connect-timeout 1000 $TRAIN_IMG_URL) >> $FILE 2>
+time (curl --connect-timeout 1000 $TRAIN_EMB_URL) >> $FILE 2>
+time (curl --connect-timeout 1000 $TEST_IMG_URL) >> $FILE 2>
+time (curl --connect-timeout 1000 $TEST_EMB_URL) >> $FILE 2>
 
 # write additional footer information to the log file
 echo "" >> $FILE
